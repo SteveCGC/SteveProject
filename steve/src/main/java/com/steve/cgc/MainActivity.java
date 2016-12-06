f4a55b773409fc8fcdb6f66596772a9c3366de0d
Kobe <changgongcai@126.com>
2016-12-06 10:03:35 +0800

    添加多个图片展示多个形状

--- steve/src/main/java/com/steve/cgc/MainActivity.java
+++ steve/src/main/java/com/steve/cgc/MainActivity.java
@@ -3,6 +3,9 @@ package com.steve.cgc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 主页面
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
